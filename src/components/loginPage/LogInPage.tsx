c1ecaa9b8998b15b872a0f821844554ae0572e2e
akash-535 <akashbish9e@gmail.com>
2025-02-12 10:00:49 +0530

    rejex complete

--- src/components/loginPage/LogInPage.tsx
+++ src/components/loginPage/LogInPage.tsx
@@ -1,19 +1,21 @@
 "use client";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 import { GoogleIcon, PageLogoIcon } from "@/utils/Icons";
-import React, { useEffect, useState } from "react";
+import Link from "next/link";
 import Image from "next/image";
 import pageImage from "../../../public/assets/images/lyriesweb-image.webp";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
 
 const LogInPage = () => {
   const logInData = {
     email: "",
     password: "",
   };
+
   const [value, setValue] = useState(logInData);
   const [error, setError] = useState(false);
   const [passwordError, setPasswordError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
@@ -23,24 +25,35 @@ const LogInPage = () => {
     }
   }, [router]);
 
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
   const pageInfoHandler = (e: any) => {
     e.preventDefault();
     setError(true);
+
+    if (value.password.length < 6) {
+      setPasswordError(true);
+    } else {
+      setPasswordError(false);
+    }
+
     if (
       value.email &&
+      emailRegex.test(value.email) &&
       value.password &&
       value.password.length >= 6 &&
-      !passwordError
+      !passwordError &&
+      !emailError
     ) {
       setError(false);
       setPasswordError(false);
+      setEmailError(false);
       setValue(logInData);
       localStorage.setItem("isAuthenticated", "true");
       router.push("/dashboard");
-    } else if (value.password.length < 6) {
-      setPasswordError(true);
     }
   };
+
   return (
     <div className="min-h-screen flex justify-center items-center py-[30px] pr-7 max-lg:pt-8 max-lg:pb-24 max-xl:px-9">
       <div className="flex w-full justify-end items-center gap-[120px] max-lg:justify-center min-[1600px]:justify-center">
@@ -55,26 +68,38 @@ const LogInPage = () => {
             <p className="text-sm font-normal leading-[30px] text-light-gray pl-0.5">
               Welcome back! Please enter your details.
             </p>
-            <form onSubmit={pageInfoHandler} className="pt-8">
+            <form className="pt-8">
               <div className="pb-0.5">
                 <p className="pb-1.5 text-base leading-5 font-medium text-again-black">
                   Email
                 </p>
                 <input
                   type="email"
-                  onChange={(e) =>
-                    setValue({ ...value, email: e.target.value })
-                  }
+                  onChange={(e) => {
+                    setValue({ ...value, email: e.target.value });
+                    if (emailRegex.test(e.target.value)) {
+                      setEmailError(false);
+                    } else {
+                      setEmailError(true);
+                    }
+                  }}
                   value={value.email}
-                  className={`placeholder:text-sm placeholder:leading-6 placeholder:font-medium text-light-gray text-sm leading-6 font-medium w-[456px] py-[19px] max-lg:w-[320px] px-3.5 border rounded-lg outline-none ${
+                  className={`placeholder:text-sm placeholder:text-light-gray placeholder:leading-6 placeholder:font-medium text-light-gray text-sm leading-6 font-medium w-[456px] py-[19px] max-lg:w-[320px] px-3.5 border rounded-lg outline-none ${
                     error && !value.email
-                      ? "placeholder:text-red-500 border-red-500"
-                      : "placeholder:text-light-gray border-light-white"
+                      ? "border-red-500"
+                      : emailError
+                      ? "border-red-500"
+                      : "border-light-white"
                   }`}
-                  placeholder={
-                    error && !value.email ? "Email is required" : "Email"
-                  }
+                  placeholder="Email"
                 />
+                <p className="text-red-500 text-sm my-1">
+                  {error && !value.email
+                    ? "Please enter your email"
+                    : emailError
+                    ? "Please check @ and . in your email"
+                    : ""}
+                </p>
               </div>
               <div className="pt-4">
                 <p className="pb-1.5 text-base leading-5 font-medium text-again-black">
@@ -128,7 +153,7 @@ const LogInPage = () => {
                 </Link>
               </div>
               <button
-                type="submit"
+                onClick={pageInfoHandler}
                 className="bg-again-black py-[8.5px] text-sm font-medium leading-6 text-white rounded-[9px] w-full max-w-[456px] mt-6 border border-transparent hover:text-black hover:bg-white hover:border-black duration-300 ease-linear max-md:mt-5"
               >
                 Sign In
